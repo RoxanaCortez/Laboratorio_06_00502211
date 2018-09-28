400ed52e3d81268b4c0883acdb046e3934a5c6f4
RoxanaCortez <00502211@uca.edu.sv>
2018-09-28 09:13:39 -0600

    :)

--- Ejercicio7.js
+++ Ejercicio7.js
@@ -1,13 +1,15 @@
 function incerteza(arreg){
-    let prom=0, suma=0; suma2=0, cuadrado=0, incerteza=0;
+    let prom=0, suma=0; suma2=0, cuadrado=0, incerteza=0, valor=0;
     for(let i; i<length.arreg; i++){
         suma=suma+arreg[i];
     }
     prom=suma/length.arreg;
     for(let i; i<length.arreg;i++){
         cuadrado=Math.pow(arreg[i]-prom,2);
-        suma2=suma+cuadrado;
+        suma2=suma2+cuadrado;
         cuadrado=0;
     }
-    incerteza=Math.sqrt()
+    valor=(1/length.arreg-1)*suma;
+    incerteza=Math.sqrt(valor);
+    console.log("La incerteza es: ", valor);
 }
